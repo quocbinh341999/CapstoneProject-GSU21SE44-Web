b6ce1da18592304c3274753c066eab5b330b2fca
BinhTQ34 <binhtqse130088@fpt.edu.vn>
2021-07-17 02:16:57 +0700

    2:16 17/7

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,7 +7,10 @@ var firebaseConfig = {
     projectId: "mumbi-app-84d15",
     storageBucket: "mumbi-app-84d15.appspot.com",
     messagingSenderId: "976794787434",
-    appId: "1:976794787434:web:727b6a4ec15bb0a23e3413"
+    appId: "1:976794787434:web:727b6a4ec15bb0a23e3413",
+    measurementId: "G-86ZMYDTB53"
 }
 
-const app = firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
+firebase.analytics();
